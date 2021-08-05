4502d4f256993b5d8836bf12674b768c6a64bb2c
kanwarote <kan_285007@hotmail.com>
2021-08-05 22:39:58 +0700

    Update MainActivity

--- app/src/main/java/com/example/wipay_iot_shop/PaymentActivity.kt
+++ app/src/main/java/com/example/wipay_iot_shop/PaymentActivity.kt
@@ -2,10 +2,21 @@ package com.example.wipay_iot_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class PaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+
+
+
+
+
+
+
+
     }
 }
