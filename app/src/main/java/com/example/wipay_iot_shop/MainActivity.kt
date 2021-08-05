503a38c556b4a44de59eed08980a7c30b6066b01
kanwarote <kan_285007@hotmail.com>
2021-08-05 22:39:58 +0700

    Update MainActivity

--- app/src/main/java/com/example/wipay_iot_shop/MainActivity.kt
+++ app/src/main/java/com/example/wipay_iot_shop/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.wipay_iot_shop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_SignIn = findViewById(com.example.wipay_iot_shop.R.id.btn_SignIn) as Button
+        val et_Email = findViewById<EditText>(R.id.et_Email)
+        val et_Password = findViewById<EditText>(R.id.et_Password)
+        btn_SignIn.setOnClickListener {
+            val email = et_Email.text.toString()
+            val password = et_Password.text.toString()
+            if (email == "wipay"&&password == "1234") {
+                startActivity(Intent(this,PaymentActivity::class.java))
+            }else{
+                Toast.makeText(this, "Invalid password or username", Toast.LENGTH_SHORT).show()
+                et_Email.setText("")
+                et_Password.setText("")
+            }
+            Log.i("test","email :"+email+"\t password :"+password)
+        }
     }
 }
