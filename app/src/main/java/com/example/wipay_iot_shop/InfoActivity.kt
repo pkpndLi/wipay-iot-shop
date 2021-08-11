2747e704368ebd941105d8bbc4ab43a119840045
pkpnd. Li <phanida.lip@wipay.co.th>
2021-08-11 09:47:48 +0700

    update Inpfo,Menu,Payment activity

--- app/src/main/java/com/example/wipay_iot_shop/InfoActivity.kt
+++ app/src/main/java/com/example/wipay_iot_shop/InfoActivity.kt
@@ -1,24 +1,87 @@
 package com.example.wipay_iot_shop
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 
-class InfoActivity : AppCompatActivity() {
+ class InfoActivity : AppCompatActivity() {
+
+     var menu:String? = null
+     var amount:Int? = null
+     var totalAmount:Int? = null
+     var quantity:Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
         val cupImg = findViewById<ImageView>(R.id.cup)
+        val menuImg = findViewById<ImageView>(R.id.img)
+        val menuName = findViewById<TextView>(R.id.menuName)
+        val payBtn = findViewById<Button>(R.id.payBtn)
+        val sumAmount = findViewById<TextView>(R.id.sumAmount)
+        val inputQuantity = findViewById<EditText>(R.id.inputQuantity)
+
+
         cupImg.setImageDrawable(getImage(this, "coffee96"))
+
+        intent.apply {
+            menu = getStringExtra("menu")
+            amount = getIntExtra("amount",145)
+        }
+
+        Log.i("logtag", menu.toString())
+
+        if(menu == "goods1"){
+              menuImg.setImageDrawable(getImage(this, "coffee"))
+              menuName.setText("S'mores Coffee Fappuccino 145B")
+        }
+        if(menu == "goods2"){
+            menuImg.setImageDrawable(getImage(this, "coffee1"))
+            menuName.setText("Neapolitan Fappuccino 145B ")
+        }
+        if(menu == "goods3"){
+            menuImg.setImageDrawable(getImage(this, "brownie"))
+            menuName.setText("Espresso ChoChip Bronie 120B")
+        }
+
+        cupImg.setOnClickListener{
+            quantity = inputQuantity.text.toString().toInt()
+            totalAmount = quantity!! * amount!!
+
+            runOnUiThread {
+                sumAmount.setText("${totalAmount}B")
+            }
+
+        }
+
+        payBtn.setOnClickListener {
+
+            val itn = Intent(this,PaymentActivity::class.java).apply{
+                putExtra("totalAmount",totalAmount)
+            }
+            startActivity(itn)
+
+        }
+
+
+
 
     }
 
-    fun getImage(context: Context, name: String?): Drawable? {
+
+
+
+     fun getImage(context: Context, name: String?): Drawable? {
         return context.getResources().getDrawable(
             context.getResources().getIdentifier(name, "drawable", context.getPackageName())
         )
     }
-}
+ }
--- app/src/main/java/com/example/wipay_iot_shop/PaymentActivity.kt
+++ app/src/main/java/com/example/wipay_iot_shop/PaymentActivity.kt
@@ -6,14 +6,18 @@ import android.widget.Button
 import android.widget.Toast
 
 class PaymentActivity : AppCompatActivity() {
+
+    var totalAmount:Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
 
+        intent.apply {
+            totalAmount = getIntExtra("totalAmount",145)
+        }
 
-
-
-
+        Toast.makeText(applicationContext,"totalAmount" + totalAmount,Toast.LENGTH_LONG).show()
 
 
 
