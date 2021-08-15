80b82e95f6ce535c2549765fe4fed3d87363fbc5
pkpnd. Li <phanida.lip@wipay.co.th>
2021-08-16 03:24:53 +0700

    create flagReverse DB

--- app/src/main/java/com/example/wipay_iot_shop/transaction/AppDatabase.kt
+++ app/src/main/java/com/example/wipay_iot_shop/transaction/AppDatabase.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.wipay_iot_shop.transaction.FlagReverseDao
+import com.example.wipay_iot_shop.transaction.FlagReverseEntity
 
-@Database(entities = arrayOf(SaleEntity::class,ReversalEntity::class),version = 1)
+@Database(entities = arrayOf(SaleEntity::class,ReversalEntity::class, FlagReverseEntity::class),version = 1)
 abstract class AppDatabase : RoomDatabase()
 {
     abstract  fun reversalDao():ReversalDao
     abstract  fun saleDao():SaleDao
+    abstract  fun flagReverseDao():FlagReverseDao
 
     companion object{
         @Volatile private var instance : AppDatabase? = null
